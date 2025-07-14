e936f2e4bf6153a66001a05f8ce089a8d7af0796
Oleg Trubnikov <oleg.trubnikov@gmail.com>
2025-07-14 16:44:09 +0700

    Fix handling of action names in `action_schemas` and update related test

--- test/action_prompt/action_schemas_test.rb
+++ test/action_prompt/action_schemas_test.rb
@@ -4,15 +4,15 @@ class DummyAgent < ApplicationAgent
   def foo
     prompt
   end
-
-  def method_name(*) = :foo
 end
 
 class ActionSchemasTest < ActiveSupport::TestCase
   test "skip missing json templates" do
     agent = DummyAgent.new
 
-    assert_nothing_raised { agent.action_schemas }
-    assert_equal [], agent.action_schemas
+    assert_nothing_raised do
+      schemas = agent.action_schemas
+      assert_equal [], schemas
+    end
   end
 end
