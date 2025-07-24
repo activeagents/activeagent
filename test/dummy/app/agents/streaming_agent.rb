d105c477987e18f112581c371313f0df74588af3
Justin Bowen <JusBowen@gmail.com>
2025-07-24 15:14:50 -0700

    Cleaning up streaming agent example

--- test/dummy/app/agents/streaming_agent.rb
+++ test/dummy/app/agents/streaming_agent.rb
@@ -5,21 +5,17 @@ class StreamingAgent < ApplicationAgent
     instructions: "You're a chat agent. Your job is to help users with their questions.",
     stream: true
 
-  on_stream do
-    # Only broadcast if we have a message with generation_id and content
-    if @_stream_message&.generation_id && @_stream_message&.content&.present?
-      broadcast_message(@_stream_message)
-    end
-  end
+  on_stream :broadcast_message
 
   private
 
-  def broadcast_message(message)
+  def broadcast_message
+    return unless @_stream_message&.generation_id && @_stream_message&.content&.present?
     # Broadcast the message to the specified channel
     ActionCable.server.broadcast(
-      "#{message.generation_id}_messages",
+      "#{@_stream_message.generation_id}_messages",
       partial: "streaming_agent/message",
-      locals: { message: message.content, scroll_to: true }
+      locals: { message: @_stream_message.content, scroll_to: true }
     )
   end
 end
