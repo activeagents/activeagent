c73a5f4a9497735d08b8bfc1cecbc8b7b025a5f7
Justin Bowen <JusBowen@gmail.com>
2025-08-21 21:40:40 -0700

    Linting

--- test/generation_provider/response_sanitization_test.rb
+++ test/generation_provider/response_sanitization_test.rb
@@ -53,7 +53,7 @@ module ActiveAgent
         # The API key should be replaced with a placeholder
         assert_equal "<OPENAI_API_KEY>", response.raw_request[:api_key]
         assert_equal "Bearer <OPENAI_API_KEY>", response.raw_request[:headers]["Authorization"]
-        
+
         # Other fields should remain unchanged
         assert_equal "gpt-4", response.raw_request[:model]
         assert_equal [ { role: "user", content: "Hello" } ], response.raw_request[:messages]
@@ -104,9 +104,9 @@ module ActiveAgent
         )
 
         # Credentials should be sanitized in array elements
-        assert_equal "You have API key: <OPENAI_API_KEY>", 
+        assert_equal "You have API key: <OPENAI_API_KEY>",
                      response.raw_request[:messages][0][:content]
-        assert_equal "What's my token <ANTHROPIC_ACCESS_TOKEN>?", 
+        assert_equal "What's my token <ANTHROPIC_ACCESS_TOKEN>?",
                      response.raw_request[:messages][1][:content]
         assert_equal "<OPENAI_API_KEY>", response.raw_request[:tools][0][:api_key]
       end
@@ -138,7 +138,7 @@ module ActiveAgent
           api_key: "sk-test123secret",
           messages: [ { role: "user", content: "Hello with key sk-test123secret" } ]
         }
-        
+
         # Keep a copy of the original to verify it wasn't modified
         original_copy = original_request.deep_dup
 
@@ -151,7 +151,7 @@ module ActiveAgent
         # Original should remain unchanged
         assert_equal original_copy, original_request
         assert_equal "sk-test123secret", original_request[:api_key]
-        
+
         # But response.raw_request should be sanitized
         assert_equal "<OPENAI_API_KEY>", response.raw_request[:api_key]
       end
@@ -171,7 +171,7 @@ module ActiveAgent
 
         assert_equal "<OPENAI_API_KEY>", response.raw_request[:openai_key]
         assert_equal "<ANTHROPIC_ACCESS_TOKEN>", response.raw_request[:anthropic_key]
-        assert_equal "Keys: <OPENAI_API_KEY> and <ANTHROPIC_ACCESS_TOKEN>", 
+        assert_equal "Keys: <OPENAI_API_KEY> and <ANTHROPIC_ACCESS_TOKEN>",
                      response.raw_request[:combined]
       end
     end
